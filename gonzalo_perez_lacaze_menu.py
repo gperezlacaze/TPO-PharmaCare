a8a5f884187329761aa19d6523a86257717db849
gperezlacaze <gperezlacaze@uade.edu.ar>
2026-06-19 14:56:27 -0300

    Mensaje

--- gonzalo_perez_lacaze_menu.py
+++ gonzalo_perez_lacaze_menu.py
@@ -406,11 +406,9 @@ def ordenar_por_vencimiento(matriz):
 
 def informe_general(matriz):
     '''Ordena y muestra el informe de medicamentos por vencimiento, con opción de ver días restantes'''
-    from lucas_bassi_menu_2 import obtener_stock_minimo
-    
     print()
     ordenar_por_vencimiento(matriz)
-    mostrar_matriz_con_colores(matriz, obtener_stock_minimo())
+    mostrar_matriz_con_colores(matriz)
     
     if validar_confirmacion("¿Desea visualizar días restantes para el vencimiento? (si/no): ") == "si":
         mostrar_dias_restantes(matriz)
