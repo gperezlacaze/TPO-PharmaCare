e101957c9bf1341e594e8afa314249bfe7b2ba70
gperezlacaze <gperezlacaze@uade.edu.ar>
2026-06-22 20:44:24 -0300

    Mensaje

--- matriz_lucas_alegre.py
+++ matriz_lucas_alegre.py
@@ -73,8 +73,8 @@ def mostrar_matriz_con_colores(matriz, stock_minimo):
 if __name__ == "__main__":
     # Prueba funcional del modulo
     matriz = crear_matriz_inicial()
-    mostrar_matriz_con_colores(matriz)
+    mostrar_matriz_con_colores(matriz, None)  
     
-    # Prueba con colores (obtiene stock_minimo internamente)
-    print("MATRIZ CON COLORES:")
-    mostrar_matriz_con_colores(matriz, None)
+    # Prueba con colores (stock mínimo = 50)
+    print("MATRIZ CON COLORES (stock mínimo = 50):")
+    mostrar_matriz_con_colores(matriz, 50)  
